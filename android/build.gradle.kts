3aa015eb2c2b6a8f57cfd056b79addeb0b1da556
arsema16 <arsema2328@gmail.com>
2026-06-01 23:53:14 +0300

    firebase setup

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    id("com.android.application") version "7.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -10,7 +16,6 @@ val newBuildDir: Directory =
         .dir("../../build")
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
-
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
